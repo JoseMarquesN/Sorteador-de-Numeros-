bb3251a966ac6194235317e2751082ae55cf51bd
José <josemarquessecundario@gmail.com>
2025-07-20 18:20:38 -0300

    Adicioando Números Sorteados e Outros

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 function sortear() {
-    let quantidade = document.getElementById("quantidade").value;
-    let de = document.getElementById("de").value;
-    let ate = document.getElementById("ate").value;
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    let sorteados = [];
+    let numero;
+
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNúmeroAleatorio(de, ate);
+        sorteados.push(numero); 
+    }
+
+    alert(sorteados);
+}
+
+function obterNúmeroAleatorio(min, max) {
+    //let randomnumeber = parseInt(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
